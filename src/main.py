ccd5c406f795be3a903381fdacd51d96b869d96f
rudaIs <kkm0476@gmail.com>
2024-05-21 21:47:17 +0900

    update molecule-editing

---
+++ src/main.py
@@ -0,0 +1,108 @@
+import graph_tool as gt
+import os
+import pathlib
+import warnings
+
+import torch
+torch.cuda.empty_cache()
+import hydra
+from omegaconf import DictConfig
+from pytorch_lightning import Trainer
+from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning.utilities.warnings import PossibleUserWarning
+
+from src import utils
+from metrics.abstract_metrics import TrainAbstractMetricsDiscrete, TrainAbstractMetrics
+
+from diffusion_model_discrete import DiscreteDenoisingDiffusion
+from diffusion.extra_features import DummyExtraFeatures, ExtraFeatures
+
+
+warnings.filterwarnings("ignore", category=PossibleUserWarning)
+
+
+def get_resume(cfg, model_kwargs):
+    """ Resumes a run. It loads previous config without allowing to update keys (used for testing). """
+    saved_cfg = cfg.copy()
+    name = cfg.general.name + '_resume'
+    resume = cfg.general.test_only
+    if cfg.model.type == 'discrete':
+        model = DiscreteDenoisingDiffusion.load_from_checkpoint(resume, **model_kwargs)
+    else:
+        model = LiftedDenoisingDiffusion.load_from_checkpoint(resume, **model_kwargs)
+    cfg = model.cfg
+    cfg.general.test_only = resume
+    cfg.general.name = name
+    cfg = utils.update_config_with_new_keys(cfg, saved_cfg)
+    return cfg, model
+
+
+@hydra.main(version_base='1.3', config_path='../configs', config_name='config')
+def main(cfg: DictConfig):
+    dataset_config = cfg["dataset"]
+
+    if dataset_config["name"] == 'qm9':
+        from metrics.molecular_metrics import TrainMolecularMetrics, SamplingMolecularMetrics
+        from metrics.molecular_metrics_discrete import TrainMolecularMetricsDiscrete
+        from diffusion.extra_features_molecular import ExtraMolecularFeatures
+        from analysis.visualization import MolecularVisualization
+        from datasets import qm9_dataset
+
+        datamodule = qm9_dataset.QM9DataModule(cfg)
+        dataset_infos = qm9_dataset.QM9infos(datamodule=datamodule, cfg=cfg)
+        train_smiles = qm9_dataset.get_train_smiles(cfg=cfg, train_dataloader=datamodule.train_dataloader(),
+                                                    dataset_infos=dataset_infos, evaluate_dataset=False)
+
+        if cfg.model.extra_features is not None:
+            extra_features = ExtraFeatures(cfg.model.extra_features, dataset_info=dataset_infos)
+            domain_features = ExtraMolecularFeatures(dataset_infos=dataset_infos)
+        else:
+            extra_features = DummyExtraFeatures()
+            domain_features = DummyExtraFeatures()
+
+        dataset_infos.compute_input_output_dims(datamodule=datamodule, extra_features=extra_features,
+                                                domain_features=domain_features)
+
+        train_metrics = TrainMolecularMetricsDiscrete(dataset_infos)
+
+        # We do not evaluate novelty during training
+        sampling_metrics = SamplingMolecularMetrics(dataset_infos, train_smiles)
+        visualization_tools = MolecularVisualization(cfg.dataset.remove_h, dataset_infos=dataset_infos)
+
+        model_kwargs = {'dataset_infos': dataset_infos, 'train_metrics': train_metrics,
+                        'sampling_metrics': sampling_metrics, 'visualization_tools': visualization_tools,
+                        'extra_features': extra_features, 'domain_features': domain_features}
+    else:
+        raise NotImplementedError("Unknown dataset {}".format(cfg["dataset"]))
+
+    if cfg.general.test_only:
+        cfg, _ = get_resume(cfg, model_kwargs)
+        os.chdir(cfg.general.test_only.split('checkpoints')[0])
+    else:
+        raise ValueError("checkpoint path does not exists")
+
+    utils.create_folders(cfg)
+
+    model = DiscreteDenoisingDiffusion(cfg=cfg, **model_kwargs)
+
+    if cfg.general.name == 'debug':
+        print("[WARNING]: Run is called 'debug' -- it will run with fast_dev_run. ")
+
+    use_gpu = cfg.general.gpus > 0 and torch.cuda.is_available()
+    trainer = Trainer(gradient_clip_val=cfg.train.clip_grad,
+                      strategy="ddp_find_unused_parameters_true",  # Needed to load old checkpoints
+                      accelerator='gpu' if use_gpu else 'cpu',
+                      devices=cfg.general.gpus if use_gpu else 1,
+                      max_epochs=cfg.train.n_epochs,
+                      check_val_every_n_epoch=cfg.general.check_val_every_n_epochs,
+                      fast_dev_run=cfg.general.name == 'debug',
+                      enable_progress_bar=False,
+                      log_every_n_steps=50 if cfg.general.name != 'debug' else 1,
+                      logger = [])
+
+    trainer.test(model, datamodule=datamodule, ckpt_path=cfg.general.test_only)
+
+
+
+if __name__ == '__main__':
+    main()
